60c93628245768df3c509451a1f5170b2472ae4e
yosser khaldi <yosserkhaldi2001@gmail.com>
2026-08-19 20:57:13 +0100

    fix: rendre EventsModule global pour resoudre l injection de dependances

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
+@Global()
 @Module({
   controllers: [EventsController],
   providers: [EventsService],
